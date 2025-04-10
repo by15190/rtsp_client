8ca6401ab5260b18c38d893b56c331b29fca9e1b
Brijesh Yadav <by15190@gmail.com>
2025-04-10 16:47:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/rtsp_client/MainActivity.kt
@@ -0,0 +1,233 @@
+package com.example.rtsp_client
+
+import android.R.attr.contentDescription
+import android.content.Context
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.InteractionSource
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.media3.common.MediaItem
+import androidx.media3.common.MimeTypes
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
+import com.example.rtsp_client.ui.theme.Rtsp_clientTheme
+
+class MainActivity : ComponentActivity() {
+    private lateinit var player: ExoPlayer
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        player = ExoPlayer.Builder(this).build() // ExoPlayer instance
+
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            var urlstate by rememberSaveable { mutableStateOf("") }
+            var recordingState by rememberSaveable { mutableStateOf(false) }
+
+            Rtsp_clientTheme {
+                val context = LocalContext.current
+                val focusManager =
+                    LocalFocusManager.current  // to manage the focus of the textField
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .clickable {
+                               focusManager.clearFocus()
+                            }
+                            .padding(innerPadding)) {
+                        searchBar(
+                            modifier = Modifier, url = urlstate, onValueChange = { urlstate = it },
+                            onSearch = {
+                                focusManager.clearFocus()
+                                val streamUrl = urlstate
+
+                                val mediaItem = MediaItem.Builder().setUri(streamUrl)
+                                    .setMimeType(MimeTypes.APPLICATION_RTSP) // Important for RTSP
+                                    .build()
+
+                                player.setMediaItem(mediaItem)
+                                player.prepare()
+                                player.play()
+                            }, context = context
+                        )
+
+                        Spacer(Modifier.height(30.dp))
+                        videoView(
+                            modifier = Modifier,
+                            url = urlstate,
+                            player = player
+                        )
+
+                        Spacer(Modifier.height(30.dp))
+                        Buttons(
+                            modifier = Modifier.padding(
+                                horizontal = 10.dp
+                            ),
+                            isRecording = recordingState,
+                            onclickRecord = { recordingState = it }
+                        )
+
+                    }
+                }
+
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        player.release()
+    }
+}
+
+@Composable
+fun searchBar(
+    modifier: Modifier, url: String, onValueChange: (String) -> Unit,
+    onSearch: () -> Unit,
+    context: Context,
+) {
+
+
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        OutlinedTextField(
+            value = url,
+            onValueChange = onValueChange,
+            label = { Text("URL") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 10.dp),
+
+            trailingIcon = {
+                IconButton(
+                    onClick = {
+                        if (url.isNotEmpty()) {
+                            onSearch()
+                        } else {
+                            Toast.makeText(context, "Please Enter a URL", Toast.LENGTH_SHORT).show()
+                        }
+
+                    }, modifier = Modifier
+                        .padding(horizontal = 10.dp)
+                        .size(50.dp)
+                ) {
+                    Icon(imageVector = Icons.Default.Search, contentDescription = null)
+                }
+            },
+            singleLine = true
+        )
+
+
+    }
+}
+
+
+@Composable
+fun Buttons(
+    modifier: Modifier,
+    isRecording: Boolean,
+    onclickRecord: (Boolean) -> Unit,
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween
+
+    ) {
+        Button(
+            onClick = {
+                onclickRecord(!isRecording)
+            },
+            modifier = Modifier
+                .padding(horizontal = 20.dp)
+                .size(150.dp, 50.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Unspecified)
+        ) {
+            Text(
+                text = if (!isRecording) "Record"
+                else "Stop", fontSize = 20.sp, fontWeight = FontWeight.SemiBold
+            )
+        }
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .padding(horizontal = 20.dp)
+                .size(150.dp, 50.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Unspecified)
+        ) {
+            Text(
+                text = "PIP", fontSize = 20.sp, fontWeight = FontWeight.SemiBold
+            )
+        }
+    }
+}
+
+@Composable
+fun videoView(
+    modifier: Modifier,
+    url: String,
+    player: ExoPlayer,
+) {
+    AndroidView(
+        factory = { context ->
+            PlayerView(context).also {
+                it.player = player
+            }
+        },
+
+        modifier = Modifier
+            .fillMaxWidth()
+            .aspectRatio(16 / 9f)
+    )
+}
+
+
